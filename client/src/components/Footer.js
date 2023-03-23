5cf4fa2dba225c7797d7a9d44f888e3d6bbb092b
Justin Hamerly <suvimountain@icloud.com>
2023-03-22 22:48:21 -0700

    project init

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          &copy; Justin Hamerly, 2023
+        </p>
+      </footer>
+    )
+  }
+}
---
+++ client/src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main>
+        <p>Search Bar Component</p>
+        <p>display component</p>
+      </main>
+    )
+  }
+}
