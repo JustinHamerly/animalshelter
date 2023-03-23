4fa541d7beeb5998ffe7e38c1fb3e96f7488c89e
Justin Hamerly <suvimountain@icloud.com>
2023-03-22 23:24:38 -0700

    search bar state and functions

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
+import SearchBar from './SearchBar'
 
 export default class Main extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      animals: [],
+      showResults: false,
+      showError: false,
+      errorMessage: ''
+    }
+  }
+
+  searchForAnimals = async (animalType) => {
+    try {
+      const url = `${process.env.REACT_APP_SERVER}/animals?type=${animalType}`;
+      const response = await axios.get(url);
+      const animalResults = response.data;
+  
+      this.setState({
+        animals: animalResults,
+        showResults: true,
+        showError: false
+      })
+
+    } catch (error) {
+      this.setState({
+        showError: true,
+        showResults: false,
+        errorMessage: error.message
+      })
+    }
+
+  }
+
+
   render() {
     return (
       <main>
-        <p>Search Bar Component</p>
+        <SearchBar
+          search={this.searchForAnimals}
+        />
         <p>display component</p>
       </main>
     )
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ''
+    }
+  }
+
+  updateSearchText = (e) => {
+    this.setState({
+      searchText: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.search(this.state.searchText)
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          
-        </form>
-      </div>
+      <Form onSubmit={this.onSubmit} id='searchForm'>
+        <Form.Group>
+          <Form.Label>Animal Type</Form.Label>
+          <Form.Control type='text' placeholder='ex: cat' onChange={this.updateSearchText} value={this.state.searchText}></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='success'>
+          Search
+        </Button>
+      </Form>
     )
   }
 }
